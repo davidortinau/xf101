c8842e837f64e695badb6b5eb30f978ced37c6d1
David Ortinau <david.ortinau@microsoft.com>
2019-08-29 18:22:25 -0500

    initial

---
+++ XF101Shell/GreenPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XF101Shell
+{
+    public partial class GreenPage : ContentPage
+    {
+        public GreenPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
